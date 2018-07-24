749f2f9c16ad644225e948205364f16f33118bbd
Julius Tens <mail@juliustens.eu>
2018-07-24 20:17:51 +0200

    temporarily disable planarization, buggy

--- prepare-graph/index.js
+++ prepare-graph/index.js
@@ -4,8 +4,8 @@ const planarize = require('./planarize')
 const addDirections = require('./add-directions')
 
 const prepareGraph = (networkGraph) => {
-    const planarGraph = planarize(networkGraph)
-    const graphWithDirections = addDirections(planarGraph)
+    // const planarGraph = planarize(networkGraph)
+    const graphWithDirections = addDirections(networkGraph)
     return graphWithDirections
 }
 
