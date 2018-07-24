99d2d61215f6b7dc16affc6702874eb11fc10e7c
Julius Tens <mail@juliustens.eu>
2018-07-24 12:58:35 +0200

    add main script

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict'
 
+const l = require('lodash')
+const pify = require('pify')
+const tmp = require('tmp')
+const path = require('path')
+const spawn = require('child-process-promise').spawn
+const fs = require('fs')
+
 const prepareGraph = require('./prepare-graph')
 const createGenerateLP = require('./generate-lp')
 const createReviseSolution = require('./revise-solution')
 
+tmp.setGracefulCleanup() // clean up even on errors
+const pTmpDir = pify(tmp.dir)
+
+// solver settings
 const settings = {
     offset: 10000,
     maxWidth: 300,
@@ -12,7 +23,13 @@ const settings = {
     maxEdgeLength: 8
 }
 
-const TransitMapSolver = (networkGraph) => {
+// script default options
+const defaults = {
+    workDir: null,
+    verbose: false
+}
+
+const Solver = (networkGraph) => {
     const graph = prepareGraph(networkGraph)
     const generateLP = createGenerateLP(graph, settings)
     const reviseSolution = createReviseSolution(graph, settings)
@@ -20,4 +37,36 @@ const TransitMapSolver = (networkGraph) => {
     return ({generateLP, reviseSolution})
 }
 
-module.exports = TransitMapSolver
+const runGurobi = (cwd, verbose=false) => {
+    // todo: escape paths?
+    const problemPath = path.resolve(cwd, 'problem.lp')
+    const solutionPath = path.resolve(cwd, 'solution.sol')
+    const gurobiPromise = spawn('gurobi_cl', [`ResultFile=${solutionPath}`, problemPath], {cwd})
+    if (verbose) gurobiPromise.childProcess.stdout.pipe(process.stderr) // sic.
+    gurobiPromise.childProcess.stderr.on('data', e => {throw new Error(e)})
+    return gurobiPromise
+}
+
+const transitMap = async (networkGraph, opt) => {
+    // prepare
+    const options = l.merge({}, defaults, opt)
+    if (!options.workDir) options.workDir = await pTmpDir({prefix: 'transit-map-'})
+
+    const solver = Solver(networkGraph)
+
+    // write problem file
+    const lpStream = fs.createWriteStream(path.resolve(options.workDir, 'problem.lp'))
+    await solver.generateLP(lpStream)
+
+    // run solver
+    await runGurobi(options.workDir, options.verbose)
+
+    // read solution file
+    const solStream = fs.createReadStream(path.resolve(options.workDir, 'solution.sol'))
+    const solution = await solver.reviseSolution(solStream)
+
+    return solution
+}
+
+module.exports = transitMap
+module.exports.Solver = Solver
